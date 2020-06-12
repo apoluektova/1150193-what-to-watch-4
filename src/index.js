43b239815d7a0655f44e9166f0e3bf730d246754
Анна Полуэктова <a.poluektova88@gmail.com>
2020-06-12 13:00:03 +0400

    Добавляет валидацию props

--- src/index.js
+++ src/index.js
@@ -2,17 +2,40 @@ import React from "react";
 import ReactDOM from "react-dom";
 import App from "./components/app/app.jsx";
 
-const Film = {
+const Movie = {
   TITLE: `The Grand Budapest Hotel`,
   GENRE: `Drama`,
   RELEASE_DATE: 2014,
 };
 
+const TITLES = [
+  `Fantastic Beasts`,
+  `Bohemian Rhapsody`,
+  `Macbeth`,
+  `Aviator`,
+  `We need to talk about Kevin`,
+  `What We Do in the Shadows`,
+  `Revenant`,
+  `Johnny English`,
+  `Shutter Island`,
+  `Pulp Fiction`,
+  `No Country for Old Men`,
+  `Snatch`,
+  `Moonrise Kingdom`,
+  `Seven Years in Tibet`,
+  `Midnight Special`,
+  `War of the Worlds`,
+  `Dardjeeling Limited`,
+  `Orlando`,
+  `Mindhunter`,
+  `Midnight Special`];
+
 ReactDOM.render(
     <App
-      title={Film.TITLE}
-      genre={Film.GENRE}
-      releaseDate={Film.RELEASE_DATE}
+      title={Movie.TITLE}
+      genre={Movie.GENRE}
+      releaseDate={Movie.RELEASE_DATE}
+      movieCardTitles={TITLES}
     />,
     document.querySelector(`#root`)
 );
