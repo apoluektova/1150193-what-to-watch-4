d8842ce97acef9283b46f909969880547f965c07
Анна Полуэктова <a.poluektova88@gmail.com>
2020-06-17 00:32:32 +0400

    Создает моки для карточки фильма

---
+++ src/mocks/films.js
@@ -0,0 +1,34 @@
+export const MOVIES = [
+  {
+    previewImage: `img/fantastic-beasts-the-crimes-of-grindelwald.jpg`,
+    title: `Fantastic Beasts: The Crimes of Grindelwald`,
+  },
+  {
+    previewImage: `img/bohemian-rhapsody.jpg`,
+    title: `Bohemian Rhapsody`,
+  },
+  {
+    previewImage: `img/macbeth.jpg`,
+    title: `Macbeth`,
+  },
+  {
+    previewImage: `img/aviator.jpg`,
+    title: `Aviator`,
+  },
+  {
+    previewImage: `mg/we-need-to-talk-about-kevin.jpg`,
+    title: `We need to talk about Kevin`,
+  },
+  {
+    previewImage: `img/what-we-do-in-the-shadows.jpg`,
+    title: `What We Do in the Shadows`,
+  },
+  {
+    previewImage: `img/revenant.jpg`,
+    title: `Revenant`,
+  },
+  {
+    previewImage: `img/johnny-english.jpg`,
+    title: `Johnny English`,
+  },
+];
