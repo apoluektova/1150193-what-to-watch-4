141976320b8e32b673d8d3a3c57205d90d303d03
Анна Полуэктова <a.poluektova88@gmail.com>
2020-06-21 12:49:10 +0400

    Исправляет логику переключения экранов

--- src/components/app/app.jsx
+++ src/components/app/app.jsx
@@ -46,7 +46,7 @@ class App extends PureComponent {
           </Route>
           <Route exact path="/movie-details">
             <MovieDetails
-              movie={this.state.currentMovieCard}
+              movie={this.state.currentMovieCard === null ? this.props.movies[0] : this.state.currentMovieCard}
             />
           </Route>
         </Switch>
@@ -65,90 +65,3 @@ App.propTypes = {
 };
 
 export default App;
-
-
-// import React, {PureComponent} from "react";
-// import PropTypes from "prop-types";
-// import {Switch, Route, BrowserRouter} from "react-router-dom";
-// import Main from "../main/main.jsx";
-// import MovieDetails from "../movie-details/movie-details.jsx";
-
-// const movieCardHoverHandler = () => {};
-
-// class App extends PureComponent {
-//   constructor(props) {
-//     super(props);
-
-//     this.state = {
-//       currentMovieCard: null,
-//     };
-
-//     this._handleMovieCardClick = this._handleMovieCardClick.bind(this);
-//   }
-
-//   _handleMovieCardClick(movieCard) {
-//     this.setState({movieCard});
-//   }
-
-//   _renderApp() {
-//     const {title, genre, releaseDate, movies} = this.props;
-//     const {currentMovieCard} = this.state;
-
-//     if (currentMovieCard) {
-//       return <MovieDetails movie={currentMovieCard} />;
-//     }
-
-//     return (
-//       <Main
-//         title={title}
-//         genre={genre}
-//         releaseDate={releaseDate}
-//         movies={movies}
-//         onMovieCardTitleClick={this._handleMovieCardClick}
-//         onMovieCardHover={movieCardHoverHandler}
-//       />
-//     );
-//   }
-
-//   render() {
-//     const {movies} = this.props;
-
-//     return (
-//       <BrowserRouter>
-//         <Switch>
-//           <Route exact path="/">
-//             {this._renderApp()}
-//           </Route>
-//           <Route exact path="/movie-details">
-//             <MovieDetails movie={movies[0]}/>
-//           </Route>
-//         </Switch>
-//       </BrowserRouter>
-//     );
-//   }
-// }
-
-
-// App.propTypes = {
-//   title: PropTypes.string.isRequired,
-//   genre: PropTypes.string.isRequired,
-//   releaseDate: PropTypes.number.isRequired,
-//   movies: PropTypes.arrayOf(
-//       PropTypes.exact({
-//         title: PropTypes.string.isRequired,
-//         backgroundImage: PropTypes.string.isRequired,
-//         poster: PropTypes.string.isRequired,
-//         genre: PropTypes.string.isRequired,
-//         releaseDate: PropTypes.number.isRequired,
-//         description: PropTypes.string.isRequired,
-//         rating: PropTypes.exact({
-//           score: PropTypes.number.isRequired,
-//           level: PropTypes.string.isRequired,
-//           count: PropTypes.number.isRequired,
-//         }),
-//         director: PropTypes.string.isRequired,
-//         actors: PropTypes.string.isRequired,
-//       })).isRequired,
-// };
-
-// export default App;
