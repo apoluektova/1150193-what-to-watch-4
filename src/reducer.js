3112fa3f3d4367ea600bb12bfc55b6df13aebf40
Анна Полуэктова <a.poluektova88@gmail.com>
2020-07-04 18:57:18 +0400

    Устанавливает Redux, добавляет и настраивает компонент GenresList

---
+++ src/reducer.js
@@ -0,0 +1,53 @@
+import {extend, getGenresList} from "./utils.js";
+import {PROMO_MOVIE, movies} from "./mocks/movies.js";
+import {reviews} from "./mocks/reviews.js";
+import {ALL_GENRES} from "./const.js";
+
+const initialState = {
+  genre: ALL_GENRES,
+  genresList: getGenresList(movies),
+  promoMovie: PROMO_MOVIE,
+  movies,
+  reviews
+};
+
+const ActionType = {
+  CHANGE_GENRE: `CHANGE_GENRE`,
+  GET_MOVIES_BY_GENRE: `GET_MOVIES_BY_GENRE`,
+};
+
+const getFilteredMovies = (genre) => movies.filter((movie) => movie.genre === genre);
+
+const ActionCreator = {
+  changeGenre: (genre) => ({
+    type: ActionType.CHANGE_GENRE,
+    payload: genre,
+  }),
+  getMoviesByGenre: (genre) => {
+    const moviesByGenre = (genre === ALL_GENRES) ? movies : getFilteredMovies(genre);
+
+    return {
+      type: ActionType.GET_MOVIES_BY_GENRE,
+      payload: moviesByGenre,
+    };
+  },
+};
+
+const reducer = (state = initialState, action) => {
+  switch (action.type) {
+
+    case (ActionType.CHANGE_GENRE):
+      return extend(state, {
+        genre: action.payload,
+      });
+
+    case (ActionType.GET_MOVIES_BY_GENRE):
+      return extend(state, {
+        movies: action.payload,
+      });
+  }
+
+  return state;
+};
+
+export {reducer, ActionType, ActionCreator};
