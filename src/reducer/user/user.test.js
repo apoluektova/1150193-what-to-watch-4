8d6de372801c4684492d63cf32781796b05c6e4f
Анна Полуэктова <a.poluektova88@gmail.com>
2020-08-07 00:26:28 +0400

    Добавляет тесты для Operations в User

--- src/reducer/user/user.test.js
+++ src/reducer/user/user.test.js
@@ -1,10 +1,18 @@
-import {AuthorizationStatus, ActionType, ActionCreator, reducer, initialState} from "./user.js";
+import {createAPI} from "../../api.js";
+import MockAdapter from "axios-mock-adapter";
+import {AuthorizationStatus, ActionType, ActionCreator, reducer, initialState, Operation} from "./user.js";
+import {createAuthorizationInfo} from "../../adapter/authInfo.js";
 
 const userInfo = {
   id: 1,
   email: `ben@mail.ru`,
   name: `Ben`,
   avatarUrl: `/wtw/static/avatar/5.jpg`,
+};
+
+const authInfo = {
+  email: `ben@gmail.com`,
+  password: `123`,
 };
 
 it(`Reducer without additional parameters should return initial state`, () => {
@@ -101,5 +109,49 @@ describe(`Action creators work correctly`, () => {
       type: ActionType.GET_SIGN_IN_ERROR,
       payload: true,
     });
+  });
+});
+
+describe(`Operation works correctly`, () => {
+  it(`Should check authorization`, () => {
+    const api = createAPI(() => {});
+
+    const apiMock = new MockAdapter(api);
+    const dispatch = jest.fn();
+    const checkAuthorization = Operation.checkAuth();
+
+    apiMock
+      .onGet(`/login`)
+      .reply(200, [{fake: true}]);
+
+    return checkAuthorization(dispatch, () => {}, api)
+          .then(() => {
+            expect(dispatch).toHaveBeenCalledTimes(2);
+            expect(dispatch).toHaveBeenCalledWith({
+              type: ActionType.REQUIRE_AUTHORIZATION,
+              payload: `AUTH`,
+            });
+          });
+  });
+
+  it(`Should post authorization info`, function () {
+    const api = createAPI(() => {});
+
+    const apiMock = new MockAdapter(api);
+    const dispatch = jest.fn();
+    const login = Operation.login(authInfo);
+
+    apiMock
+      .onPost(`/login`)
+      .reply(200, [{fake: true}]);
+
+    return login(dispatch, () => {}, api)
+      .then(() => {
+        expect(dispatch).toHaveBeenCalledTimes(2);
+        expect(dispatch).toHaveBeenCalledWith({
+          type: ActionType.GET_AUTHORIZATION_INFO,
+          payload: createAuthorizationInfo({fake: true}),
+        });
+      });
   });
 });
